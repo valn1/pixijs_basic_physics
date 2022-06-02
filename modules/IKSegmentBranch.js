e825b283b65164a96a1e2f920230febf67f1307b
Valdeli Vaz <prog57@ixcsoft.com.br>
2022-06-02 14:06:53 -0300

    feat:  added support to textures

--- modules/IKSegmentBranch.js
+++ modules/IKSegmentBranch.js
@@ -8,6 +8,10 @@ export default class branch {
         this.getPointB();
     }
 
+    setTexture(texture) {
+        this.texture = texture;
+    }
+
     create(){
         this.line = new PIXI.Graphics()
         this.line.lineStyle(8, 0xFFFF00, 1);
@@ -15,7 +19,18 @@ export default class branch {
         this.getPointB();
         this.line.lineTo(this.bx, this.by);
         globalThis.app.stage.addChild(this.line);
+        this.skin();
         return this.line;
+    }
+
+    skin(){
+        this.setTexture(PIXI.Texture.from('../assets/textures/arm.png'));
+        this.scale = new PIXI.Sprite(this.texture);
+        this.scale.anchor.set(0.5,0);
+        this.scale.pivot.set(0.5,0);
+        this.scale.width=50;
+        this.scale.height=this.length*2;
+        globalThis.app.stage.addChild(this.scale);
     }
 
     calculatePointA(){
@@ -25,6 +40,11 @@ export default class branch {
             ((this.parent.by-this.parent.ay)/Math.sqrt((this.parent.bx - this.parent.ax)** 2+(this.parent.by - this.parent.ay)** 2)))
     }
 
+    getOffset(angle,length){
+        this.offx=Math.cos(angle)*length;
+        this.offy=Math.sin(angle)*length;
+    }
+
     calculatePointB(){
         this.bx=(this.ax+this.length*((this.bx-this.ax)/Math.sqrt((this.bx - this.ax)** 2+(this.by - this.ay)** 2)))
         this.by=(this.ay+this.length*((this.by-this.ay)/Math.sqrt((this.bx - this.ax)** 2+(this.by - this.ay)** 2)))
@@ -32,20 +52,25 @@ export default class branch {
 
     update(){
         this.line.clear()
-        this.line.lineStyle(8, 0xFFFF00, 1, .5, false);
+        // this.line.lineStyle(8, 0xFFFF00, 1, .5, false);
         this.calculatePointA()
         this.line.position.set(this.ax,this.ay)
         this.calculatePointB()
         this.line.lineTo(this.bx-this.ax,this.by-this.ay)
-        // console.log(this.line.position)
+        this.scale.rotation = this.getAngle(this.parent.ax,this.parent.ay,this.parent.bx,this.parent.by);
+        this.getOffset(this.angle+(90*(Math.PI/180)),25)
+        this.scale.position.set(this.ax+this.offx,this.ay+this.offy)
     }
 
-    // essa função usa o angulo pra determinar o ponto b, só precisamos determinar o ângulo, se esse angulo for maior que o ângulo máximo, getpointB
-    //o ângulo deve ser relativo ao parent dele
     getPointB(){
         this.bx=(this.ax+(this.length*Math.cos(this.angle)))
         this.by=(this.ay+(this.length*Math.sin(this.angle)))
         // this.angle=Math.asin(((this.ay-this.by)/this.length))
+    }
+
+    getAngle(ax,ay,bx,by){
+        this.angle=Math.atan2(by-ay,bx-ax)
+        return this.angle;
     }
 
 }
--- modules/Segment.js
+++ modules/Segment.js
@@ -23,16 +23,31 @@ export default class {
         this.setBByAngle(this.angle||90);
         this.line.lineTo(this.bx, this.by);
         globalThis.app.stage.addChild(this.line);
+        this.skin();
         return this.line;
+    }
+
+    skin(){
+        this.setTexture(PIXI.Texture.from('../assets/textures/scales.jpg'));
+        // this.texture.trim=new PIXI.Rectangle(0,0,50,this.length);
+        this.scale = new PIXI.Sprite(this.texture);
+        this.scale.anchor.set(0.5,0);
+        this.scale.pivot.set(0.5,0);
+        this.scale.width=50;
+        this.scale.height=this.length;
+        // this.scale.frame=new PIXI.Rectangle(0,0,50,this.length);
+        globalThis.app.stage.addChild(this.scale);
     }
 
     update(){
         this.line.clear();
-        this.line.lineStyle(2, this.color, 1);
+        // this.line.lineStyle(2, this.color, 1);
         this.setPointA();
         this.line.moveTo(this.ax,this.ay);
         this.calculatePointB()
         this.line.lineTo(this.bx,this.by);
+        this.scale.rotation = this.getAngle(this.ax,this.ay,this.bx,this.by)-(90 * (Math.PI/180));
+        this.scale.position.set(this.ax,this.ay);
     }
 
     setPointA(){
@@ -48,6 +63,10 @@ export default class {
     setBByAngle(angle){
         this.bx = this.ax + this.length * Math.cos(angle);
         this.by = this.ay + this.length * Math.sin(angle);
+    }
+
+    getAngle(ax,ay,bx,by){
+        return Math.atan2(by-ay,bx-ax);
     }
 
     calculatePointB(){
